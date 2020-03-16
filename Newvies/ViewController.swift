e140a5f03345b6c30a584d1deb35bc8763af37c9
Jay Jac <jy_jac@msn.com>
2020-03-15 21:21:59 -0400

    Initial Commit

---
+++ Newvies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Newvies
+//
+//  Created by Jay Jac on 3/15/20.
+//  Copyright © 2020 Jacaria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
